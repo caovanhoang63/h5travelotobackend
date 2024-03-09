b2817dd78c760a2f31e93bf50a53ec950c41a1d6
caovanhoang63ubt <22520457@gm.uit.edu.vn>
2024-03-09 14:05:32 +0700

    Add Gin handler function for user login process.

---
+++ module/users/transport/ginuser/login.go
@@ -0,0 +1,37 @@
+package ginuser
+
+import (
+	"github.com/gin-gonic/gin"
+	"h5travelotobackend/common"
+	"h5travelotobackend/component/appContext"
+	"h5travelotobackend/component/hasher"
+	"h5travelotobackend/component/tokenprovider/jwt"
+	userbiz "h5travelotobackend/module/users/business"
+	usermodel "h5travelotobackend/module/users/model"
+	userstorage "h5travelotobackend/module/users/storage"
+	"net/http"
+)
+
+func UserLogin(appCtx appContext.AppContext) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var userLogin usermodel.UserLogin
+		err := c.ShouldBind(&userLogin)
+		if err != nil {
+			panic(common.ErrInvalidRequest(err))
+		}
+
+		db := appCtx.GetGormDbConnection()
+		tokenProvider := jwt.NewJWTProvider(appCtx.GetSecretKey()) //appctx.SecretKey()
+		store := userstorage.NewSqlStore(db)
+		sha256Hasher := hasher.NewSha256Hash()
+		biz := userbiz.NewLoginBiz(appCtx, store, tokenProvider, sha256Hasher, 60*60*24*30)
+
+		account, err := biz.Login(c.Request.Context(), &userLogin)
+
+		if err != nil {
+			panic(err)
+		}
+
+		c.JSON(http.StatusOK, common.SimpleSuccessResponse(account))
+	}
+}
