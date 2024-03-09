d268a51a65e8958c15aadb08ab3c2f74e1452aad
caovanhoang63ubt <22520457@gm.uit.edu.vn>
2024-03-09 12:36:54 +0700

    Add UID struct and methods for generating and decomposing unique identifiers.

--- common/sql_model.go
+++ common/sql_model.go
@@ -1,1 +1,16 @@
 package common
+
+import "time"
+
+type SqlModel struct {
+	Id        int        `json:"-" gorm:"column:id"`
+	FakeId    *UID       `json:"id" gorm:"-"`
+	Status    int        `json:"status" gorm:"column:status;default:1"`
+	CreatedAt *time.Time `json:"created_at,omitempty" gorm:"column:created_at"`
+	UpdatedAt *time.Time `json:"updated_at,omitempty" gorm:"column:updated_at"`
+}
+
+func (m *SqlModel) GenUID(dbType int) {
+	uid := NewUID(uint32(m.Id), dbType, 0)
+	m.FakeId = &uid
+}
