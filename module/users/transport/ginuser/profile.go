651492cfa45213fe065addd31d754b0f5ec41f6f
caovanhoang63ubt <22520457@gm.uit.edu.vn>
2024-03-09 14:05:46 +0700

    Add Gin handler function for retrieving user profile.

---
+++ module/users/transport/ginuser/profile.go
@@ -0,0 +1,16 @@
+package ginuser
+
+import (
+	"github.com/gin-gonic/gin"
+	"h5travelotobackend/common"
+	"h5travelotobackend/component/appContext"
+	"net/http"
+)
+
+func GetProfile(appCtx appContext.AppContext) func(*gin.Context) {
+	return func(c *gin.Context) {
+		data := c.MustGet(common.CurrentUser).(common.Requester)
+
+		c.JSON(http.StatusOK, common.SimpleSuccessResponse(data))
+	}
+}
