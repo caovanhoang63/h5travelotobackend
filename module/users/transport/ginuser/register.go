99e9a3fd3a3e2694ba49e609a18d79e7a47878c4
caovanhoang63ubt <22520457@gm.uit.edu.vn>
2024-03-09 14:05:56 +0700

    Add new file for registering users in Gin framework.

---
+++ module/users/transport/ginuser/register.go
@@ -0,0 +1,37 @@
+package ginuser
+
+import (
+	"github.com/gin-gonic/gin"
+	"h5travelotobackend/common"
+	"h5travelotobackend/component/appContext"
+	"h5travelotobackend/component/hasher"
+	userbiz "h5travelotobackend/module/users/business"
+	usermodel "h5travelotobackend/module/users/model"
+	userstorage "h5travelotobackend/module/users/storage"
+)
+
+func RegisterUser(appctx appContext.AppContext) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var data usermodel.UserCreate
+
+		if err := c.ShouldBind(&data); err != nil {
+			panic(common.ErrInvalidRequest(err))
+		}
+
+		db := appctx.GetGormDbConnection()
+
+		store := userstorage.NewSqlStore(db)
+
+		sha256Hasher := hasher.NewSha256Hash()
+
+		biz := userbiz.NewRegisterBiz(store, sha256Hasher)
+
+		if err := biz.Register(c.Request.Context(), &data); err != nil {
+			panic(err)
+		}
+
+		data.Mask(false)
+
+		c.JSON(200, common.SimpleSuccessResponse(data.FakeId.String()))
+	}
+}
