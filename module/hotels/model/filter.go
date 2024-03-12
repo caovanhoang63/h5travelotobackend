a1160545d1664dc001a55349b0256ca078faf8a9
caovanhoang63ubt <22520457@gm.uit.edu.vn>
2024-03-12 09:16:41 +0700

    Fixed validation logic in Filter struct and added error handling in the Validate method.

--- module/hotels/model/filter.go
+++ module/hotels/model/filter.go
@@ -1,5 +1,10 @@
 package hotelmodel
 
+import (
+	"errors"
+	"github.com/asaskevich/govalidator"
+)
+
 type Filter struct {
 	OwnerId    int     `json:"owner_id" form:"owner"`
 	ProvinceID int     `json:"province_id" gorm:"column:province_id" form:"province"`
@@ -9,3 +14,20 @@ type Filter struct {
 	Lat        float64 `json:"lat" form:"lat"`
 	Lng        float64 `json:"lng" form:"lng"`
 }
+
+func (f *Filter) Validate() error {
+	if (f.Distance+f.Lng+f.Lat) != 0 && (f.Distance == 0 || f.Lat == 0 || f.Lng == 0) {
+		return ErrInvalidLocation
+	}
+	if f.Lat != 0 && f.Lng != 0 {
+		if !govalidator.IsLatitude(govalidator.ToString(f.Lat)) {
+			return ErrInvalidLocation
+		}
+		if !govalidator.IsLongitude(govalidator.ToString(f.Lng)) {
+			return ErrInvalidLocation
+		}
+	}
+	return nil
+}
+
+var ErrInvalidLocation = errors.New("invalid location")
