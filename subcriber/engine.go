6f500e117cac724bdf06a9f616ae848e0d1b8b7c
caovanhoang63ubt <22520457@gm.uit.edu.vn>
2024-03-15 09:56:58 +0700

    Add subscriber engine implementation with consumer job setup and job handling logic.

---
+++ subcriber/engine.go
@@ -0,0 +1,66 @@
+package subcriber
+
+import (
+	"context"
+	"h5travelotobackend/common"
+	"h5travelotobackend/component/appContext"
+	"h5travelotobackend/component/asyncjob"
+	"h5travelotobackend/component/pubsub"
+	"log"
+)
+
+type consumerJob struct {
+	Title   string
+	Handler func(ctx context.Context, message *pubsub.Message) error
+}
+
+type consumerEngine struct {
+	appCtx appContext.AppContext
+}
+
+func NewEngine(appCtx appContext.AppContext) *consumerEngine {
+	return &consumerEngine{appCtx: appCtx}
+}
+
+func (engine *consumerEngine) Start() error {
+
+	return nil
+}
+
+type GroupJob interface {
+	Run(ctx context.Context) error
+}
+
+func (engine *consumerEngine) startSubTopic(topic string, isConcurrent bool, consumerJobs ...consumerJob) error {
+	c, _ := engine.appCtx.GetPubSub().Subscribe(context.Background(), topic)
+	for _, item := range consumerJobs {
+		log.Printf("Set up consumer for: %s", item.Title)
+	}
+
+	getJobHandler := func(job *consumerJob, message *pubsub.Message) asyncjob.JobHandler {
+		return func(ctx context.Context) error {
+			log.Printf("running job for %s for. Value %s \n", job.Title, message.Data)
+			return job.Handler(ctx, message)
+		}
+	}
+
+	go func() {
+		common.AppRecover()
+		for {
+			msg := <-c
+			jobHdlArr := make([]asyncjob.Job, len(consumerJobs))
+
+			for i := range consumerJobs {
+				jobHdl := getJobHandler(&consumerJobs[i], msg)
+				jobHdlArr[i] = asyncjob.NewJob(jobHdl)
+			}
+			group := asyncjob.NewGroup(isConcurrent, jobHdlArr...)
+
+			if err := group.Run(context.Background()); err != nil {
+				log.Println("Err:", err)
+			}
+		}
+	}()
+
+	return nil
+}
