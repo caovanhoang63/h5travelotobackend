4d727caac3261a0aa140c2a00dc7f466290f53cc
caovanhoang63ubt <22520457@gm.uit.edu.vn>
2024-03-22 17:49:34 +0700

    comment all

--- mockdata/main.go
+++ mockdata/main.go
@@ -1,35 +1,27 @@
 package main
 
-import (
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-	"log"
-	"math/rand"
-	"time"
-)
-
 func main() {
 
-	mySqlConnString := "h5traveloto:04rosftx61jb8md@tcp(localhost:3306)/h5traveloto?charset=utf8mb4&parseTime=True&loc=Local"
-
-	db, err := gorm.Open(mysql.Open(mySqlConnString), &gorm.Config{})
-	if err != nil {
-		log.Fatal(db, err)
-	}
-	s1 := rand.NewSource(time.Now().UnixNano())
-
-	r1 := rand.New(s1)
-
-	db = db.Debug()
-	db = db.Table("rooms")
-	query := "insert into rooms(hotel_id, room_type_id, name) VALUES (5,?,'aaaa');"
-	for j := 0; j < 10; j++ {
-		db.Begin()
-		for i := 0; i < 10000; i++ {
-			a := r1.Intn(3) + 12
-			db.Where("in (?)")
-		}
-		db.Commit()
-	}
+	//mySqlConnString := "h5traveloto:04rosftx61jb8md@tcp(localhost:3306)/h5traveloto?charset=utf8mb4&parseTime=True&loc=Local"
+	//
+	//db, err := gorm.Open(mysql.Open(mySqlConnString), &gorm.Config{})
+	//if err != nil {
+	//	log.Fatal(db, err)
+	//}
+	//s1 := rand.NewSource(time.Now().UnixNano())
+	//
+	//r1 := rand.New(s1)
+	//
+	//db = db.Debug()
+	//db = db.Table("rooms")
+	//query := "insert into rooms(hotel_id, room_type_id, name) VALUES (5,?,'aaaa');"
+	//for j := 0; j < 10; j++ {
+	//	db.Begin()
+	//	for i := 0; i < 10000; i++ {
+	//		a := r1.Intn(3) + 12
+	//		db.Where("in (?)")
+	//	}
+	//	db.Commit()
+	//}
 
 }
