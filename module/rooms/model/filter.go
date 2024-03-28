4badd41dcc26fdcbe6c05605a94057089639915d
caovanhoang63ubt <22520457@gm.uit.edu.vn>
2024-03-28 17:37:15 +0700

    add: UnMask function

--- module/rooms/model/filter.go
+++ module/rooms/model/filter.go
@@ -1,9 +1,24 @@
 package roommodel
 
+import "h5travelotobackend/common"
+
 type Filter struct {
-	HotelId    int `json:"hotel_id" form:"hotel_id"`
-	RoomTypeId int `json:"room_type_id" form:"room_type_id"`
-	Status     int `json:"status" form:"status"`
+	HotelId        int         `json:"-" form:"-"`
+	HotelFakeID    *common.UID `json:"hotel-id" form:"hotel-id"`
+	RoomTypeId     int         `json:"-" form:"-"`
+	RoomTypeFakeId *common.UID `json:"room-type-id" form:"room-type-id"`
+	Status         int         `json:"status" form:"status"`
+}
+
+func (f *Filter) UnMask() *Filter {
+	if f.HotelFakeID != nil {
+		f.HotelId = int(f.HotelFakeID.GetLocalID())
+	}
+	if f.RoomTypeFakeId != nil {
+		f.RoomTypeId = int(f.RoomTypeFakeId.GetLocalID())
+	}
+	return f
+
 }
 
 func (f *Filter) SetDefault() {
