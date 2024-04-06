79190770084d542e3f8c648855e22c8c27f9c532
caovanhoang63ubt <22520457@gm.uit.edu.vn>
2024-04-06 17:53:12 +0700

    add : hotel detail model

--- module/hoteldetails/model/facility.go
+++
@@ -1,8 +0,0 @@
-package hoteldetailmodel
-
-type Facility struct {
-	Name       string          `json:"name" bson:"name"`
-	Categories map[string]bool `json:"categories,omitempty" bson:"categories,omitempty"`
-}
-
-type FacilityList []Facility
--- module/hoteldetails/model/model.go
+++ module/hoteldetails/model/model.go
@@ -4,63 +4,53 @@ import (
 	"h5travelotobackend/common"
 )
 
-/*
-	{
-	  "_id" : "2131231231232123",
-	  "hotel_id": 1,
-	  "logo": {
-		"id": 1,
-		"url": "http://example.com/logo.jpg",
-		"width": 100,
-		"height": 100,
-		"cloud_name": "s3",
-		"extension": "jpg"
-	  },
-	  "covers": [],
-	  "policies": {
-		"check_in_time": "2018-01-01T12:00:00Z",
-		"check_out_time": "2018-01-01T12:00:00Z",
-		"required_documents": "ID Card",
-		"deposit": 100,
-		"min_age": 18
-	  },
-	  "facilities": [
-		{
-		  "name": "common",
-		  "categories": {
-			"wifi": true,
-			"parking": true
-		  }
-		},
-		{
-		  "name": "general",
-		  "categories": {
-			"wifi": true,
-			"parking": true
-		  }
-		}
-	  ]
-	}
-
-*/
-
 const EntityName = "HotelDetail"
 
+// HotelDetail struct
 type HotelDetail struct {
-	common.MongoModel `bson:",inline" json:",inline"`
-	HotelId           int            `json:"-" bson:"hotel_id"`
-	Logo              *common.Image  `json:"logo,omitempty" bson:"logo,omitempty" `
-	Covers            *common.Images `json:"covers,omitempty" bson:"covers,omitempty" `
-	Policies          *Policies      `json:"policies,omitempty" bson:"policies,omitempty"`
-	Facilities        *FacilityList  `json:"facilities,omitempty" bson:"facilities,omitempty"`
-	Description       *Description   `bson:"description" json:"description"`
+	common.SqlModel      `json:",inline"`
+	HotelId              int     `json:"hotel_id" gorm:"column:hotel_id"`
+	NumberOfFloor        int     `json:"number_of_floor" gorm:"column:number_of_floor"`
+	DistanceToCenterCity float64 `json:"distance_to_center_city" gorm:"column:distance_to_center_city"`
+	Description          string  `json:"description" gorm:"column:description"`
+	LocationDetail       string  `json:"location_detail" gorm:"column:location_detail"`
+	CheckInTime          string  `json:"check_in_time" gorm:"column:check_in_time"`
+	CheckOutTime         string  `json:"check_out_time" gorm:"column:check_out_time"`
+	RequireDocument      string  `json:"require_document" gorm:"column:require_document"`
+	MinimumAge           int     `json:"minimum_age" gorm:"column:minimum_age"`
+	CancellationPolicy   float32 `json:"cancellation_policy" gorm:"column:cancellation_policy"`
+	SmokingPolicy        string  `json:"smoking_policy" gorm:"column:smoking_policy"`
+	PetPolicy            string  `json:"pet_policy" gorm:"column:pet_policy"`
+	AdditionalPolicy     string  `json:"additional_policy" gorm:"column:additional_policy"`
 }
 
-type Description struct {
-	LocationDetails string `json:"location_details" bson:"location_details"`
-	About           string `bson:"about" json:"about"`
-}
-
-func (HotelDetail) CollectionName() string {
+// TableName func
+func (HotelDetail) TableName() string {
 	return "hotel_details"
+}
+
+type HotelDetailCreate HotelDetail
+
+func (HotelDetailCreate) TableName() string {
+	return HotelDetail{}.TableName()
+}
+
+type HotelDetailUpdate struct {
+	HotelId              int     `json:"hotel_id" gorm:"column:hotel_id"`
+	NumberOfFloor        int     `json:"number_of_floor" gorm:"column:number_of_floor"`
+	DistanceToCenterCity float64 `json:"distance_to_center_city" gorm:"column:distance_to_center_city"`
+	Description          string  `json:"description" gorm:"column:description"`
+	LocationDetail       string  `json:"location_detail" gorm:"column:location_detail"`
+	CheckInTime          string  `json:"check_in_time" gorm:"column:check_in_time"`
+	CheckOutTime         string  `json:"check_out_time" gorm:"column:check_out_time"`
+	RequireDocument      string  `json:"require_document" gorm:"column:require_document"`
+	MinimumAge           int     `json:"minimum_age" gorm:"column:minimum_age"`
+	CancellationPolicy   float32 `json:"cancellation_policy" gorm:"column:cancellation_policy"`
+	SmokingPolicy        string  `json:"smoking_policy" gorm:"column:smoking_policy"`
+	PetPolicy            string  `json:"pet_policy" gorm:"column:pet_policy"`
+	AdditionalPolicy     string  `json:"additional_policy" gorm:"column:additional_policy"`
+}
+
+func (HotelDetailUpdate) TableName() string {
+	return HotelDetail{}.TableName()
 }
