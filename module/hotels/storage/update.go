4b2831facdbfb0f5e5c61c00c87c6fa94636b26a
caovanhoang63ubt <22520457@gm.uit.edu.vn>
2024-04-07 23:03:13 +0700

    add: functions to update hotel (rating, room-type, price)when review or room type delete

--- module/hotels/storage/update.go
+++ module/hotels/storage/update.go
@@ -27,6 +27,19 @@ func (s *sqlStore) UpdateReviewWhenUserReview(ctx context.Context, review *commo
 	return nil
 }
 
+func (s *sqlStore) UpdateReviewWhenDeleteReview(ctx context.Context, review *common.DTOReview) error {
+	db := s.db.Table(hotelmodel.Hotel{}.TableName()).Where("id = ?", review.HotelId)
+
+	db = db.Update("rating", gorm.Expr("(rating * total_rating - ?)/(total_rating - ?)", review.Rating, 1))
+	db = db.Update("total_rating", gorm.Expr("total_rating - ?", 1))
+
+	if err := db.Error; err != nil {
+		return common.ErrDb(err)
+	}
+
+	return nil
+}
+
 func (s *sqlStore) UpdateTotalRoomTypeAndAvgPrice(ctx context.Context, room *common.DTORoomType) error {
 	db := s.db.Table(hotelmodel.Hotel{}.TableName()).Where("id = ?", room.HotelId)
 
@@ -39,4 +52,17 @@ func (s *sqlStore) UpdateTotalRoomTypeAndAvgPrice(ctx context.Context, room *com
 
 	return nil
 
+}
+
+func (s *sqlStore) UpdateTotalRoomTypeAndAvgPriceWhenDeleteRoomType(ctx context.Context, room *common.DTORoomType) error {
+	db := s.db.Table(hotelmodel.Hotel{}.TableName()).Where("id = ?", room.HotelId)
+
+	db = db.Update("avg_price", gorm.Expr("(avg_price * total_room_type - ?)/(total_room_type - ?)", room.Price, 1))
+	db = db.Update("total_room_type", gorm.Expr("total_room_type - ?", 1))
+
+	if err := db.Error; err != nil {
+		return common.ErrDb(err)
+	}
+
+	return nil
 }
