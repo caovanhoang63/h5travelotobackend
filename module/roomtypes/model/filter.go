a35d06ac68a935ffa71424a891d64f5c5622f374
caovanhoang63ubt <22520457@gm.uit.edu.vn>
2024-05-08 14:21:27 +0700

    Refactor roomtypes filter model to fix typos and set default max price if not provided.

--- module/roomtypes/model/filter.go
+++ module/roomtypes/model/filter.go
@@ -6,20 +6,21 @@ import (
 
 type Filter struct {
 	HotelId     int         `json:"-" gorm:"column:hotel_id;" form:"-"`
-	HotelFakeId common.UID  `json:"hotel_id" gorm:"-" form:"hotel-id"`
+	HotelFakeId common.UID  `json:"hotel_id" gorm:"-" form:"hotel_id"`
 	Bed         *common.Bed `json:"bed" gorm:"column:bed;" form:"bed"`
 	Area        float64     `json:"area" gorm:"column:area;" form:"area"`
-	PayInHotel  bool        `json:"pay_in_hotel" gorm:"column:pay_in_hotel;" form:"pay-in-hotel"`
-	BreakFast   bool        `json:"break_fast" gorm:"column:break_fast;" form:"break-fast"`
-	FreeCancel  bool        `json:"free_cancel" gorm:"column:free_cancel;" form:"free-cancel"`
-	MaxPrice    float64     `json:"max_price"  form:"max-price"`
-	MinPrice    float64     `json:"min_price" form:"min-price"`
-	MaxCustomer int         `json:"max_customer" gorm:"column:max_customer;" form:"max-customer"`
+	PayInHotel  bool        `json:"pay_in_hotel" gorm:"column:pay_in_hotel;" form:"pay_in_hotel"`
+	BreakFast   bool        `json:"break_fast" gorm:"column:break_fast;" form:"break_fast"`
+	FreeCancel  bool        `json:"free_cancel" gorm:"column:free_cancel;" form:"free_cancel"`
+	MaxPrice    float64     `json:"max_price"  form:"max_price" `
+	MinPrice    float64     `json:"min_price" form:"min_price" `
+	MaxCustomer int         `json:"max_customer" gorm:"column:max_customer;" form:"max_customer"`
 }
 
 func (f *Filter) SetDefault() {
-	f.MaxPrice = 10000000000
-	f.MinPrice = 0
+	if f.MaxPrice == 0 {
+		f.MaxPrice = 10000000000
+	}
 }
 
 func (f *Filter) UnMask() {
