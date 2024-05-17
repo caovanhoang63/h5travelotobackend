1d71508eb536cbc455f3afcd4beb9b9ac2c93dc0
caovanhoang63ubt <22520457@gm.uit.edu.vn>
2024-05-18 00:44:42 +0700

    Refactor model.go to use common.Hotel struct instead of custom Hotel struct in Booking type

--- module/bookings/model/model.go
+++ module/bookings/model/model.go
@@ -9,7 +9,7 @@ const EntityName = "Booking"
 type Booking struct {
 	common.SqlModel  `json:",inline"`
 	HotelId          int                `json:"-" gorm:"column:hotel_id"`
-	Hotel            *Hotel             `json:"hotel,omitempty" gorm:"foreignKey:HotelId;preload:false"`
+	Hotel            *common.Hotel      `json:"hotel,omitempty" gorm:"foreignKey:HotelId;preload:false"`
 	UserFakeId       *common.UID        `json:"user_id"`
 	UserId           int                `json:"-" gorm:"column:user_id"`
 	User             *common.SimpleUser `json:"user,omitempty" gorm:"foreignKey:UserId;preload:false"`
@@ -86,55 +86,3 @@ var (
 		"room type is invalid",
 		"ERR_INVALID_ROOM_TYPE")
 )
-
-type Hotel struct {
-	common.SqlModel `json:",inline"`
-	Name            string         `json:"name" gorm:"column:name"`
-	Address         string         `json:"address" gorm:"column:address"`
-	HotelType       int            `json:"-" gorm:"column:hotel_type"`
-	Logo            *common.Image  `json:"logo" gorm:"column:logo"`
-	Images          *common.Images `json:"images" gorm:"column:images"`
-	ProvinceCode    int            `json:"-" gorm:"column:province_code"`
-	Province        *Province      `json:"province,inline" gorm:"foreignKey:ProvinceCode;references:Code"`
-	DistrictCode    int            `json:"-" gorm:"column:district_code"`
-	District        *District      `json:"district,inline" gorm:"foreignKey:DistrictCode;references:Code"`
-	WardCode        int            `json:"-" gorm:"column:ward_code"`
-	Ward            *Ward          `json:"ward,inline" gorm:"foreignKey:WardCode;references:Code"`
-	Star            int            `json:"star" gorm:"star"`
-	TotalRating     int            `json:"total_rating" gorm:"total_rating"`
-}
-
-func (Hotel) TableName() string {
-	return "hotels"
-}
-
-func (data *Hotel) Mask(isAdmin bool) {
-	data.GenUID(common.DbTypeHotel)
-}
-
-type Province struct {
-	Code int    `json:"code" gorm:"column:code"`
-	Name string `json:"name" gorm:"column:name"`
-}
-
-func (Province) TableName() string {
-	return "provinces"
-}
-
-type District struct {
-	Code int    `json:"code" gorm:"column:code"`
-	Name string `json:"name" gorm:"column:name"`
-}
-
-func (District) TableName() string {
-	return "districts"
-}
-
-type Ward struct {
-	Code int    `json:"code" gorm:"column:code"`
-	Name string `json:"name" gorm:"column:name"`
-}
-
-func (Ward) TableName() string {
-	return "wards"
-}
